6983bfd23417b4b563a4569cfdf00f92a96aa4d0
XD-Ooi <43445880+XD-Ooi@users.noreply.github.com>
2018-12-17 20:59:48 +0800

    Update wcount.py

--- pyassign4/wcount.py
+++ pyassign4/wcount.py
@@ -6,7 +6,6 @@ wcount.py: This module provides a function to calculate the frequency of the wor
 that appears in a text file from a given url. The user inputs the target url and
 the number of most frequent words (default = 10) to be displayed from the command prompt.
 """
-
 import sys
 from urllib.request import urlopen
 import string
@@ -15,7 +14,6 @@ def retrieve_file(url):
     """
     The function retrieves the text file from a given url.
     """
-    
     doc = urlopen(url)
     lines = doc.read().decode()
     doc.close()
@@ -30,7 +28,6 @@ def wcount(lines,topn = 10):
     the words and the frequency they appear in the text file. Then, the function
     ranks the words and output the n most frequent words that appear in the text file.
     """
-    
     word_dict = {}
     line = lines.replace("-"," ")
     to_remove = string.punctuation + string.whitespace
@@ -49,8 +46,13 @@ def wcount(lines,topn = 10):
 
     #print the list with the top n most frequent words.
     output_list = []
-    for freq,word in arranged[:topn]:
-        output_list.append((word,freq))
+    if topn >= len(arranged):
+        for freq,word in arranged:
+            output_list.append((word,freq))
+    else:
+        for freq,word in arranged[:topn]:
+            output_list.append((word,freq))
+    
     for word,freq in output_list:
         print(word,"\t",freq)
     return output_list    
@@ -65,16 +67,17 @@ if __name__ == "__main__":
         paras = sys.argv
         url = paras[1]
         if len(paras) > 2:
-            topn = int(paras[2])
+            try:
+                topn = int(paras[2])
+            except Exception as err1:
+                print(err1, "\n","Will run the function with default topn = 10")
+        
         try:
             lines = retrieve_file(url)
-        except Exception as err:
-            print(err)
+        except Exception as err2:
+            print(err2)
         else:
             try:
                 wcount(lines,topn)
             except:
-                wcount(lines)
-            
-      
-            
+                wcount(lines)           
